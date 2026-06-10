e3533eaa49c9ad9fb1bafe669bc1f29558dc907a
Juan David Leon <juan.garces.leon@correounivalle.edu.co>
2026-06-10 01:06:55 -0500

    cors login

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 const allowedOrigins = [
     "http://localhost:5173",
-    "https://cafe-aroma-rock.vercel.app/"
+    "https://cafe-aroma-rock.vercel.app/login"
 ];
 
 app.use(morgan("dev"));
